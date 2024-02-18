b504c7e4bca01844a1d9010601a2d954c0e6b3e2
PhantomOz <faniogor@gmail.com>
2024-02-18 21:26:15 +0100

    This commit will add constructor logic

--- src/MultiSig.sol
+++ src/MultiSig.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.19;
 
 contract MultiSigWallet {
-    mapping(uint256 => mapping(address => Transaction))
-        private idToAdressTransaction;
     address[] public owners;
     mapping(address => bool) public addressIsOwner;
     uint256 public numOfConfirmationRequired;
+    mapping(uint256 => mapping(address => bool)) private transactionIsConfirmed;
 
     struct Transaction {
         address to;
@@ -16,5 +15,30 @@ contract MultiSigWallet {
         uint256 numOfConfirmation;
     }
 
-    constructor(address[] memory _owner, uint256 _numOfConfirmationRequired) {}
+    constructor(address[] memory _owners, uint256 _numOfConfirmationRequired) {
+        if (_owners.length == 0) {
+            revert();
+        }
+        if (
+            _numOfConfirmationRequired == 0 ||
+            _numOfConfirmationRequired > _owners.length
+        ) {
+            revert();
+        }
+
+        for (uint256 i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+            if (owner == address(0)) {
+                revert();
+            }
+            if (addressIsOwner[owner]) {
+                revert();
+            }
+
+            addressIsOwner[owner] = true;
+            owners = _owners;
+        }
+
+        numOfConfirmationRequired = _numOfConfirmationRequired;
+    }
 }
