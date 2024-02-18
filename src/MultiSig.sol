03589717e891810786405653d1236167f035a656
PhantomOz <faniogor@gmail.com>
2024-02-18 21:34:40 +0100

    This commit will add constructor custom errors

--- src/MultiSig.sol
+++ src/MultiSig.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+error OWNERSLIST_CANT_BE_EMPTY();
+error NO_CONFIRMATION_MUST_BE_IN_RANGE_WITH_LIST();
+error ADDRESS_CANT_BE_ZEROADDRESS();
+error ADDRESSES_ON_LIST_NOT_UNIQUE();
+
 contract MultiSigWallet {
     address[] public owners;
     mapping(address => bool) public addressIsOwner;
@@ -17,22 +22,22 @@ contract MultiSigWallet {
 
     constructor(address[] memory _owners, uint256 _numOfConfirmationRequired) {
         if (_owners.length == 0) {
-            revert();
+            revert OWNERSLIST_CANT_BE_EMPTY();
         }
         if (
             _numOfConfirmationRequired == 0 ||
             _numOfConfirmationRequired > _owners.length
         ) {
-            revert();
+            revert NO_CONFIRMATION_MUST_BE_IN_RANGE_WITH_LIST();
         }
 
         for (uint256 i = 0; i < _owners.length; i++) {
             address owner = _owners[i];
             if (owner == address(0)) {
-                revert();
+                revert ADDRESS_CANT_BE_ZEROADDRESS();
             }
             if (addressIsOwner[owner]) {
-                revert();
+                revert ADDRESSES_ON_LIST_NOT_UNIQUE();
             }
 
             addressIsOwner[owner] = true;
