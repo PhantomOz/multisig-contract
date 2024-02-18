ec18566c252f0630f79bcd4d783fc62fe6ba8191
PhantomOz <faniogor@gmail.com>
2024-02-18 21:14:20 +0100

    This commit will add some state variables and constructor

--- src/MultiSig.sol
+++ src/MultiSig.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MultiSig {}
+contract MultiSigWallet {
+    mapping(uint256 => mapping(address => Transaction))
+        private idToAdressTransaction;
+    address[] public owners;
+    mapping(address => bool) public addressIsOwner;
+    uint256 public numOfConfirmationRequired;
+
+    struct Transaction {
+        address to;
+        bytes data;
+        uint256 value;
+        bool isExecuted;
+        uint256 numOfConfirmation;
+    }
+
+    constructor(address[] memory _owner, uint256 _numOfConfirmationRequired) {}
+}
